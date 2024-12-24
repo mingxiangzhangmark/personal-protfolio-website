0c714ba44bff4e512d758435e6a9568cc358a252
Mark.Zhang <mingxiangzhang_mark@outlook.com>
2024-12-25 01:21:47 +1100

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 
-import Button from "@/components/ui/button";
-import Card from "@/components/ui/card";
-import {  FaArrowRight, FaHome, FaUser } from "react-icons/fa";
 // import AboutSection from "@/sections/about";
 // import ContactSection from "@/sections/contact";
 // import FeaturedSection from "@/sections/featured";
-// import LandingSection from "@/sections/landing";
+import LandingSection from "@/sections/landing";
 import dynamic from "next/dynamic";
-import Input from "@/components/ui/input";
-import TextArea from "@/components/ui/text-area";
-import Profile from "@/components/ui/profile";
-import FancyButton from "@/components/ui/fancy-button";
-import LiveClock from "@/components/ui/live-clock";
-import MagnetciWrapper from "@/components/visualEffects/magnetic-wrapper";
-import ScrollDown from "@/components/ui/scroll-down";
+
 
 
 // Dynamic import WaterWaveWrapper to ensure it's only used in the client-side environment
@@ -32,41 +23,14 @@ export default function Home() {
       resolution="2048"
     >
       {() => (
-        // <div className="pb-8">
-
-
-        <div className="w-full p-10">
-         <div className="max-w-2xl mx-auto">
-          <Card title="UI components">
-            <div className="flex justify-between">
-              <Button>Basic Button</Button>
-             <Button link="https://google.com">Link Button</Button>
-             <Button isIcon><FaUser/></Button>
-             <Button><FaHome/> Home Page</Button>
-            </div>
-            <Input type="text" name="exampleInput" placeholder="Write some things"/>
-            <Input type="text" name="exampleInput" placeholder="Write some things" icon={<FaUser/>}/>
-            <TextArea name="exampleTextArea" placeholder="Write some things" icon = {<FaHome/>}/>
-            <TextArea name="exampleTextArea" placeholder="Write some things"/>
-            <Profile/>
-            <MagnetciWrapper className="w-[300px]">
-              <FancyButton text="Let's Talk" icon={<FaArrowRight/>}/>
-            </MagnetciWrapper>
-            <LiveClock timeZone="Australia/Sydney"/>
-            <MagnetciWrapper>
-              <ScrollDown/>
-            </MagnetciWrapper>
-             
-          </Card>
-         </div>
+        <div className="pb-8">
         
 
-
-
-          {/* <LandingSection />
-          <FeaturedSection />
-          <AboutSection />
-          <ContactSection /> */}
+          <LandingSection />
+          <LandingSection />
+          {/* <FeaturedSection /> */}
+          {/* <AboutSection /> */}
+          {/* <ContactSection /> */}
         </div>
       )}
     </WaterWaveWrapper>
---
+++ sections/landing.tsx
@@ -0,0 +1,53 @@
+import Header from "@/components/navigation/header/header";
+import FancyButon from "@/components/ui/fancy-button";
+import LiveClock from "@/components/ui/live-clock";
+import ScrollDown from "@/components/ui/scroll-down";
+import MagnetciWrapper from "@/components/visualEffects/magnetic-wrapper";
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+
+export default function LandingSection() {
+  return (
+    <div className="relative h-screen overflow-hidden p-8">
+      {/*Header*/}
+      <Header />
+      {/*Show magnetic fancy button on small screens and hide it on md screen*/}
+      <div className="absolute bottom-36 left-10 z-20 md:hidden">
+        <MagnetciWrapper>
+          <FancyButon text="Let's talk" icon={<FaArrowRight />} />
+        </MagnetciWrapper>
+      </div>
+      {/*Live clock*/}
+      <div className="absolute right-10 bottom-10">
+        <LiveClock timeZone="Australia/Sydney" />
+      </div>
+      {/*Slogan*/}
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8 leading-[14vw] lg:leading-[10vw] 2xl:leading-[9rem] font-medium h-[40rem] tracking-[-0.3rem]">
+        <div className="flex flex-col justify-center items-center text-primary-foreground text-[16vw] lg:text-[12vw] 2xl:text-[10rem] uppercase">
+          <div>
+            <span>Code</span>
+          </div>
+          <div>
+            <span>Creation</span>
+          </div>
+          <div className="relative">
+            <span>Expertise</span>
+            <div className="text-[1rem] leading-[1.4rem]  tracking-[-0.07rem] absolute top-[14vw] lg:top-[10vw] 2xl:top-[9rem] left-0 2xl:left-[57rem] w-[30rem] uppercase font-normal">
+              <span>Building solutions</span>
+              <br />
+              <span>through seamless coding</span>
+              <br />
+              <span>where innovation drives results</span>
+              <br />
+              <span>and ideas transform into reality.</span>
+            </div>
+          </div>
+        </div>
+        {/*Magentic scroll down*/}
+        <MagnetciWrapper className="absolute left-1/2 -translate-x-1/2 bottom-[8rem] md:bottom-[4rem] 2xl:-bottom-10 ">
+          <ScrollDown />
+        </MagnetciWrapper>
+      </div>
+    </div>
+  );
+}
